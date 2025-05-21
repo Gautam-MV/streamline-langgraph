cf05c863bf929fdd3b2c600f9d3eb6a6fcc58170
Gautam2000 <63543004+Gautam-MV@users.noreply.github.com>
2025-05-22 03:48:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import socket
 import time
 import psutil
 from graph_utils import build_graph, State
-from ui_preview import save_and_preview_generated_ui
+from ui_renderer import save_and_preview_generated_ui
 from streamlit_drawable_canvas import st_canvas
 import numpy as np
 from image_groq_tools import set_groq_api_key
